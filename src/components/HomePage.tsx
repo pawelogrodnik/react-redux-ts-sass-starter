ee7c0cd82f52f467195ce60b26e4033a7d6e3183
Paweł Ogrodnik <mkpawell@gmail.com>
2019-05-30 19:21:43 +0200

    dodanie fonta do projektu

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import HeroBanner from './HeroBanner';
 import IntroTextBanner from './IntroTextBanner';
 import SearchInvestmentsForm from './Forms/SearchInvestmentsForm';
-import { InvestmentList, InvestmentBlock } from './InvestmentBlock';
+import { InvestmentList } from './InvestmentBlock';
 
 export default class HomePage extends React.Component<any, any> {
     public render() {
         const list = new Array(3).fill(true);
         return (
             <div className="page page--homepage">
-                {/* <HeroBanner backgroundImageSrc={''} title={'Lorem ipsum'} description={'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'} buttonText={'Lorem ipsum'}/>
+                <HeroBanner backgroundImageSrc={''} title={'Lorem ipsum'} description={'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'} buttonText={'Lorem ipsum'}/>
                 <IntroTextBanner title={'Wyszukiwarka inwestycji'} description={'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'}/>
                 <SearchInvestmentsForm onSubmit={(data)=>console.log(data)}/>
-                <InvestmentList list={list}/> */}
+                <InvestmentList list={list}/>
             </div>
         )
     }
